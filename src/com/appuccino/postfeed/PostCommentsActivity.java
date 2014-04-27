a9be32b9800b1b3aaf6754961d21b6dbefa5c16e
Waylon Brown <waylonjbrown@gmail.com>
2014-04-26 22:44:05 -0500

    arrows in comments activity update

--- src/com/appuccino/postfeed/PostCommentsActivity.java
+++ src/com/appuccino/postfeed/PostCommentsActivity.java
@@ -1,24 +1,29 @@
 package com.appuccino.postfeed;
 
-import com.appuccino.postfeed.MainActivity.NewPostFragment;
-import com.appuccino.postfeed.MainActivity.TopPostFragment;
-import com.appuccino.postfeed.listadapters.CommentListAdapter;
-import com.appuccino.postfeed.objects.Post;
-
 import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Html;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AbsListView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.appuccino.postfeed.MainActivity.NewPostFragment;
+import com.appuccino.postfeed.MainActivity.TopPostFragment;
+import com.appuccino.postfeed.listadapters.CommentListAdapter;
+import com.appuccino.postfeed.objects.NetWorker;
+import com.appuccino.postfeed.objects.Post;
+import com.appuccino.postfeed.objects.Vote;
 
 public class PostCommentsActivity extends Activity{
 
 	static CommentListAdapter listAdapter;
+	Post post;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +40,6 @@ public class PostCommentsActivity extends Activity{
 		actionBar.setIcon(R.drawable.logofake);
 		
 		int sectionNumber = getIntent().getIntExtra("SECTION_NUMBER", 0);
-		Post post;
 		if(sectionNumber == 0)
 			post = TopPostFragment.getPostByID(getIntent().getIntExtra("POST_ID", -1), sectionNumber);
 		else
@@ -54,13 +58,12 @@ public class PostCommentsActivity extends Activity{
 		scoreText.setTypeface(bold);
 		messageText.setTypeface(light);
 		timeText.setTypeface(lightItalic);
-		commentsText.setTypeface(medium);
+		commentsText.setTypeface(light);
 		
 		ListView commentsListView = (ListView)findViewById(R.id.commentsList);
 		if(post != null)
 		{
 			scoreText.setText(String.valueOf(post.getScore()));
-			messageText.setText(post.getMessage());
 			timeText.setText(String.valueOf(post.getHoursAgo()) + " hours ago");
 			
 			listAdapter = new CommentListAdapter(this, R.layout.list_row_card, post.getCommentList());
@@ -74,7 +77,91 @@ public class PostCommentsActivity extends Activity{
 				commentsListView.addFooterView(headerFooter, null, false);
 			}
 			commentsListView.setAdapter(listAdapter);
+			
+			setMessageAndColorizeTags(post.getMessage(), messageText);
+			final ImageView arrowUp = (ImageView)findViewById(R.id.arrowUp);
+			final ImageView arrowDown = (ImageView)findViewById(R.id.arrowDown);
+	        //arrow click listeners
+	        arrowUp.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					//if already upvoted, un-upvote
+					if(post.getVote() != 1)
+						post.setVote(1);
+					else
+						post.setVote(0);
+					TopPostFragment.updateList();
+					NewPostFragment.updateList();
+					TagListActivity.updateList();
+					updateArrows(arrowUp, arrowDown);
+					new NetWorker.VoteTask().execute(new Vote(post.getID(), true));
+				}        	
+	        });
+	        arrowDown.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					//if already downvoted, un-downvote
+					if(post.getVote() != -1)
+						post.setVote(-1);
+					else
+						post.setVote(0);
+					TopPostFragment.updateList();
+					NewPostFragment.updateList();
+					TagListActivity.updateList();
+					updateArrows(arrowUp, arrowDown);
+				}        	
+	        });
+	        
+	        updateArrows(arrowUp, arrowDown);
 		}
+	}
+	
+	protected void updateArrows(ImageView arrowUp, ImageView arrowDown) 
+	{
+		int vote = post.getVote();
+        if(vote == -1)
+        {
+        	arrowDown.setImageDrawable(getResources().getDrawable(R.drawable.arrowdownred));
+        	arrowUp.setImageDrawable(getResources().getDrawable(R.drawable.arrowup));
+        }
+        else if (vote == 1)
+        {
+        	arrowUp.setImageDrawable(getResources().getDrawable(R.drawable.arrowupblue));
+        	arrowDown.setImageDrawable(getResources().getDrawable(R.drawable.arrowdown));
+        }
+        else	//no votes
+        {
+        	arrowUp.setImageDrawable(getResources().getDrawable(R.drawable.arrowup));
+        	arrowDown.setImageDrawable(getResources().getDrawable(R.drawable.arrowdown));
+        }
+	}
+
+	void setMessageAndColorizeTags(String msg, TextView messageText) 
+    {
+    	String tagColor = "#33B5E5";
+    	String message = msg;
+    	
+    	String[] wordArray = message.split(" ");
+    	//check for tags, colorize them
+    	for(int i = 0; i < wordArray.length; i++)
+    	{
+    		if(wordArray[i].substring(0, 1).equals("#") && wordArray[i].length() > 1)
+    		{
+    			wordArray[i] = "<font color='" + tagColor + "'>" + wordArray[i] + "</font>";
+    		}
+    	}
+    	
+    	message = "";
+    	//combine back to string
+    	for(int i = 0; i < wordArray.length; i++)
+    	{
+    		message += wordArray[i] + " ";
+    	}
+    	
+    	messageText.setText(Html.fromHtml(message));
+		
 	}
 
 	@Override
