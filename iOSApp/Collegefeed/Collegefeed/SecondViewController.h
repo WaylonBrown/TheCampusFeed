56a70d3e31aa0034b90b66db7a112b2bdc0e2061
Patrick Sheehan <patrickanthony.sheehan@yahoo.com>
2014-05-01 22:05:08 -0500

    initial template - iOS tabbed application

---
+++ iOSApp/Collegefeed/Collegefeed/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Collegefeed
+//
+//  Created by Patrick Sheehan on 5/1/14.
+//  Copyright (c) 2014 Appuccino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
