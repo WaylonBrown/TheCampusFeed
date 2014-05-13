4a0b45583079c5e6ee3320ea2eec78df7e928a16
bowmessage <bowmessage@gmail.com>
2014-05-12 23:14:56 -0700

    Added college_id reference field so each post has a college.

--- RailsApp/db/schema.rb
+++ RailsApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506041550) do
+ActiveRecord::Schema.define(version: 20140513060759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,10 @@ ActiveRecord::Schema.define(version: 20140506041550) do
     t.float    "lon"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "college_id"
   end
+
+  add_index "posts", ["college_id"], name: "index_posts_on_college_id", using: :btree
 
   create_table "votes", force: true do |t|
     t.boolean  "upvote"
