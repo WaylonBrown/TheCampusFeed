1c5e49baaa5506dc6c101cb6c0c3dd04946a7fbc
Waylon Brown <waylonjbrown@gmail.com>
2014-05-17 00:55:34 -0500

    temp commit

--- AndroidApp/src/com/appuccino/collegefeed/fragments/MyCommentsFragment.java
+++ AndroidApp/src/com/appuccino/collegefeed/fragments/MyCommentsFragment.java
@@ -16,9 +16,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.appuccino.collegefeed.MainActivity;
 import com.appuccino.collegefeed.PostCommentsActivity;
