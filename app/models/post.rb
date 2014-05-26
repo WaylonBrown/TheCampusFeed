918e0e167ddb0bfa36c15df0e6b790e4a6f25bf9
bowmessage <bowmessage@gmail.com>
2014-05-26 15:26:31 -0700

    Refactored and modified post creation and tag detection. Added code for new endpoints.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,22 @@ class Post < ActiveRecord::Base
   belongs_to :college, inverse_of: :posts
 
   validates :text, length: {minimum: 10, maximum: 100}
+
+  after_save :make_tags
+
+  def make_tags
+    @words = text.split(/[\r\n\t ]+/)
+    @words.each do |w|
+      if w.length > 0 && w[0] == "#" then
+        @text = w[1..-1]
+        @existing = Tag.find_by text: @text
+        if @existing.nil?
+          @tag = tags.build({text: @text})
+          @tag.save
+        elsif !tags.include? @existing
+          tags << @existing
+        end
+      end
+    end
+  end
 end
