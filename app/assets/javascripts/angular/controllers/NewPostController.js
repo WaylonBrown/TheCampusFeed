81e5c230b0ec36893d4c204cb05d78b80a5f0bff
bowmessage <bowmessage@gmail.com>
2014-06-09 23:14:13 -0700

    Lots of separation of angular js into its own files and dir structure. Got admin interface to have an add post module. Typeahead grabs list of colleges for auto complete.

---
+++ app/assets/javascripts/angular/controllers/NewPostController.js
@@ -0,0 +1,34 @@
+angular.module('cfeed')
+  .controller('NewPostController', ['$scope', '$http', '$modalInstance', function($scope, $http, $modalInstance) {
+
+  $scope.newPost = {}
+  $scope.allColleges = []
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.newPost);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  }
+
+  $scope.getAllColleges = function(){
+    $http.get('api/v1/colleges')
+      .then(function(res){
+        $scope.allColleges = res.data
+      })
+  }
+
+  $scope.getAllColleges()
+  
+  $scope.getColleges = function() {
+    return $http.get('api/v1/colleges')
+      .then(function(res){
+        console.log(res.data)
+        return res.data
+      })
+  }
+}]);
+
+
+
---
+++ app/assets/javascripts/angular/main.js
@@ -0,0 +1,5 @@
+angular.module('cfeed', ["ui.bootstrap"])
+
+angular.element(document).ready(function () {
+  //$("i.tip").tooltip();
+});
