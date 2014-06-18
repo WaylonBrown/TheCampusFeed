f5e039d60f4f104c14b49c80fd83ab0b3585e1cc
bowmessage <bowmessage@gmail.com>
2014-06-17 22:20:21 -0700

    Fix college search on admin page.

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,6 +1,6 @@
 class College < ActiveRecord::Base
   include PgSearch
-  pg_search_scope :search_by_text, :against => :text
+  pg_search_scope :search_by_text, :against => :name
 
   has_many :posts, inverse_of: :college
 end
