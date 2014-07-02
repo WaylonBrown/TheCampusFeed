f2a72a0cd3a37d77433868e16578e40dee3ff9f0
Waylon Brown <waylonjbrown@gmail.com>
2014-07-01 21:30:26 -0500

    Improved splash animation

--- AndroidApp/src/com/appuccino/collegefeed/SplashActivity.java
+++ AndroidApp/src/com/appuccino/collegefeed/SplashActivity.java
@@ -6,15 +6,22 @@ import java.util.TimerTask;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends Activity{
 	
-	private int SPLASH_MILLISECONDS = 1000;
+	private int SPLASH_MILLISECONDS = 500;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
 		setContentView(R.layout.activity_splash);
+		LinearLayout layout = (LinearLayout) findViewById(R.id.splashLayout);
+		AlphaAnimation animation = new AlphaAnimation(0.0f , 1.0f ) ;
+		animation.setFillAfter(true);
+		animation.setDuration(SPLASH_MILLISECONDS);
+		//apply the animation ( fade In ) to your LAyout
+		layout.startAnimation(animation);
 		runTimer();
 	}
 
@@ -37,9 +44,8 @@ public class SplashActivity extends Activity{
 	
 	private void goToMainActivity(){
 		Intent intent = new Intent(this, MainActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION );
 		startActivity(intent);
-		finish();
 		overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);	
+		finish();
 	}
 }
