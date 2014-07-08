21cba8aec8f373352280ea6ca8c486fdced2bdb2
Waylon Brown <waylonjbrown@gmail.com>
2014-07-07 19:11:27 -0500

    MyContent post list loads with a fake list

--- AndroidApp(AS)/app/src/main/java/com/appuccino/collegefeed/MyContentActivity.java
+++ AndroidApp(AS)/app/src/main/java/com/appuccino/collegefeed/MyContentActivity.java
@@ -4,12 +4,20 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.appuccino.collegefeed.adapters.PostListAdapter;
+import com.appuccino.collegefeed.objects.Comment;
+import com.appuccino.collegefeed.objects.Post;
 import com.appuccino.collegefeed.utils.FontManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Waylon on 7/6/2014.
@@ -18,6 +26,9 @@ public class MyContentActivity extends Activity{
 
     ListView myPostsListView;
     ListView myCommentsListView;
+    public static PostListAdapter listAdapter;
+    public static List<Post> postList = new ArrayList<Post>();
+    public static List<Comment> commentList = new ArrayList<Comment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +36,59 @@ public class MyContentActivity extends Activity{
         setContentView(R.layout.my_content);
         setupActionbar();
         setupViews();
+        setupList();
         fetchLists();
         //TODO: remove this
+        updateLists();
+    }
+
+    private void setupList() {
+        listAdapter = new PostListAdapter(this, R.layout.list_row_collegepost, postList, 0, MainActivity.ALL_COLLEGES);
+        myPostsListView = (ListView)findViewById(R.id.myPostsListView);
+        if(myPostsListView != null)
+            myPostsListView.setAdapter(listAdapter);
+        else
+            Log.e("cfeed", "MyContentPostList list adapter wasn't set.");
+
+        myPostsListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int position, long arg3)
+            {
+                postClicked(postList.get(position - 1));
+            }
+        });
+    }
+
+    private void postClicked(Post post) {
     }
 
     private void fetchLists() {
         //TODO: fetch from server here
+
     }
 
+    public static void updateLists() {
+        //TODO: remove these manual lists
+        List<Post> testList = new ArrayList<Post>();
+        testList.add(new Post("test", 1));
+        testList.add(new Post("test", 1));
+        testList.add(new Post("test", 1));
+        testList.add(new Post("test", 1));
+        postList = new ArrayList<Post>(postList);
+        postList.addAll(testList);
 
+        if(listAdapter != null)
+        {
+            Log.i("cfeed", "TEST new post size: " + postList.size());
+            listAdapter.setCollegeFeedID(MainActivity.ALL_COLLEGES);
+            listAdapter.clear();
+            listAdapter.addAll(postList);
+            Log.i("cfeed","TEST last post size: " + listAdapter.getCount());
+            listAdapter.notifyDataSetChanged();
+        }
+    }
 
     private void setupActionbar() {
         ActionBar actionBar = getActionBar();
@@ -55,5 +110,9 @@ public class MyContentActivity extends Activity{
 
         myPostsText.setTypeface(FontManager.light);
         myCommentsText.setTypeface(FontManager.light);
+    }
+
+    public static void makeLoadingIndicator(boolean makeLoading){
+        //TODO
     }
 }
