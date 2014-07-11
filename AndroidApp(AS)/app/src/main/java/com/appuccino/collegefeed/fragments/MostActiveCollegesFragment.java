3baaddeb22fda702870fce421400d4b071215cf4
Waylon Brown <waylonjbrown@gmail.com>
2014-07-10 19:17:54 -0500

    Load trending college list from server

--- AndroidApp(AS)/app/src/main/java/com/appuccino/collegefeed/fragments/MostActiveCollegesFragment.java
+++ AndroidApp(AS)/app/src/main/java/com/appuccino/collegefeed/fragments/MostActiveCollegesFragment.java
@@ -1,5 +1,7 @@
 package com.appuccino.collegefeed.fragments;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,18 +10,22 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.appuccino.collegefeed.MainActivity;
 import com.appuccino.collegefeed.R;
 import com.appuccino.collegefeed.adapters.FragmentCollegeListAdapter;
 import com.appuccino.collegefeed.objects.College;
+import com.appuccino.collegefeed.utils.NetWorker;
 
 import java.util.ArrayList;
 
 public class MostActiveCollegesFragment extends Fragment
 {
 	static MainActivity mainActivity;
-	static ArrayList<College> collegeList;
+	public static ArrayList<College> collegeList;
+    private static ProgressBar progressSpinner;
+    static FragmentCollegeListAdapter adapter;
 
 	public MostActiveCollegesFragment()
 	{
@@ -36,30 +42,45 @@ public class MostActiveCollegesFragment extends Fragment
 		View rootView = inflater.inflate(R.layout.fragment_layout,
 				container, false);
 		ListView fragList = (ListView)rootView.findViewById(R.id.fragmentListView);
-		
+        progressSpinner = (ProgressBar)rootView.findViewById(R.id.loadingSpinner);
+
 		disableFooter(rootView);
 		
 		collegeList = new ArrayList<College>();
-		collegeList.add(new College("Texas A&M University", 1422));
-		collegeList.add(new College("Harvard University", 1066));
-		collegeList.add(new College("University of Texas at Austin", 1423));
 		
-		FragmentCollegeListAdapter adapter = new FragmentCollegeListAdapter(getActivity(), R.layout.list_row_college, collegeList, true);
-		
-		//if doesnt have header and footer, add them
-		if(fragList.getHeaderViewsCount() == 0)
-		{
-			//for card UI
-			View headerFooter = new View(getActivity());
-			headerFooter.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, 8));
-			fragList.addFooterView(headerFooter, null, false);
-			fragList.addHeaderView(headerFooter, null, false);
-		}
-	    fragList.setAdapter(adapter);
-	    fragList.setItemsCanFocus(true);
-	    
+		adapter = new FragmentCollegeListAdapter(getActivity(), R.layout.list_row_college, collegeList, true);
+
+        if(fragList != null) {
+            //if doesnt have header and footer, add them
+            if(fragList.getHeaderViewsCount() == 0)
+            {
+                //for card UI
+                View headerFooter = new View(getActivity());
+                headerFooter.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, 8));
+                fragList.addFooterView(headerFooter, null, false);
+                fragList.addHeaderView(headerFooter, null, false);
+            }
+            fragList.setAdapter(adapter);
+            fragList.setItemsCanFocus(true);
+        }
+
+        pullListFromServer();
+
 		return rootView;
 	}
+
+    private static void pullListFromServer()
+    {
+        if(collegeList == null){
+            collegeList = new ArrayList<College>();
+        }
+        ConnectivityManager cm = (ConnectivityManager) mainActivity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(cm.getActiveNetworkInfo() != null){
+            new NetWorker.GetCollegeFragmentTask().execute(new NetWorker.PostSelector());
+        } else {
+            makeLoadingIndicator(false);
+        }
+    }
 
 	private void disableFooter(View root) {
 		LinearLayout footer = (LinearLayout)root.findViewById(R.id.footer);
@@ -71,4 +92,21 @@ public class MostActiveCollegesFragment extends Fragment
         mainActivity.goToTopPostsAndScrollToTop();
         mainActivity.changeFeed(thisCollege.getID());
 	}
+
+    public static void updateList() {
+        if(adapter != null)
+        {
+            adapter.clear();
+            adapter.addAll(collegeList);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public static void makeLoadingIndicator(boolean b) {
+        if(b){
+            progressSpinner.setVisibility(View.VISIBLE);
+        }else{
+            progressSpinner.setVisibility(View.GONE);
+        }
+    }
 }
