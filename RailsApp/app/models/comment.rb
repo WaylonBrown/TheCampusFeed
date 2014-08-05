43124c76305dcd5e4a824bf93f16930111928725
James Bowman <bowmessage@gmail.com>
2014-08-05 15:14:49 -0700

    Parse tags from comments too.

--- RailsApp/app/models/comment.rb
+++ RailsApp/app/models/comment.rb
@@ -2,6 +2,25 @@ class Comment < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search_by_text, :against => :text
 
+
+  after_save :make_tags
+
+  def make_tags
+    @words = text.split(/[\r\n\t ]+/)
+    @words.each do |w|
+      if w.length > 0 && w[0] == "#" then
+        @text = w[1..-1]
+        @existing = Tag.find_by text: @text
+        if @existing.nil?
+          @tag = tags.create({text: @text})
+        elsif !tags.include? @existing
+          tags << @existing
+        end
+      end
+    end
+  end
+
+
   belongs_to :post
   has_many :votes, as: :votable
   has_many :flags, as: :flaggable
