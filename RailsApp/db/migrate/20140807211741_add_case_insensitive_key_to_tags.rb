fc12558da0cf9bfda179f7acfaec72991238718f
James Bowman <bowmessage@gmail.com>
2014-08-14 17:37:26 -0500

    Switching from pg to mysql b/c of Rackspace not supporting pg.

--- RailsApp/db/migrate/20140807211741_add_case_insensitive_key_to_tags.rb
+++ RailsApp/db/migrate/20140807211741_add_case_insensitive_key_to_tags.rb
@@ -1,9 +1,10 @@
 class AddCaseInsensitiveKeyToTags < ActiveRecord::Migration
+  #this migration doesn't work on mysql, switching to mysql means I have to disable it.
   def up
-    execute("CREATE INDEX lower_tag_name ON tags(lower(text))")
+    #execute("CREATE INDEX lower_tag_name ON tags(lower(text))")
   end
 
   def down
-    execute("DROP INDEX lower_tag_name")
+    #execute("DROP INDEX lower_tag_name")
   end
 end
