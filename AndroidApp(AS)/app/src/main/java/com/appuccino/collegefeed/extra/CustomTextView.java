ca6b7f53374287873407c7dbf526342df5100c77
Waylon Brown <waylonjbrown@gmail.com>
2014-08-15 18:44:56 -0500

    Made TextViews awesome

---
+++ AndroidApp(AS)/app/src/main/java/com/appuccino/collegefeed/extra/CustomTextView.java
@@ -0,0 +1,56 @@
+package com.appuccino.collegefeed.extra;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.appuccino.collegefeed.R;
+import com.appuccino.collegefeed.utils.FontManager;
+
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+
+    }
+
+    public CustomTextView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs!=null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+            String fontName = a.getString(R.styleable.CustomTextView_typeface);
+            if (fontName!=null) {
+                Typeface tf = null;
+                if(fontName.equals("italic")){
+                    tf = FontManager.italic;
+                } else if (fontName.equals("medium")){
+                    tf = FontManager.medium;
+                } else if (fontName.equals("bold")){
+                    tf = FontManager.bold;
+                } else if (fontName.equals("thin")){
+                    tf = FontManager.thin;
+                } else {
+                    //this is the fallback font if none is defined
+                    tf = FontManager.light;
+                }
+                setTypeface(tf);
+                //sets default color for if textColor isn't already defined in xml
+                setTextColor(getResources().getColor(R.color.darkgray));
+            }
+            a.recycle();
+        }
+    }
+
+}
