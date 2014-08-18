860e6dfa165384f041d5ad53a69c8ab7c7a54006
James Bowman <bowmessage@gmail.com>
2014-08-17 22:52:00 -0500

    Correctly set the casedText value before validation of its existence.

--- RailsApp/app/models/tag.rb
+++ RailsApp/app/models/tag.rb
@@ -1,17 +1,29 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :posts
+  has_and_belongs_to_many :comments
+
   scope :top, ->(college, lim) {
     cid = {}
     if college
       cid["posts"] = {college_id: college.id}
     end
-    select("tags.id, tags.text, count(posts.id) AS posts_count").
+
+    select("tags.id, tags.text, count(posts.id) AS posts_count, count(comments.id) AS comments_count").
     joins(:posts).
+    joins(:comments).
     where(cid).
     group("tags.id").
     order("posts_count DESC").
     limit(lim) }
 
+  validates_presence_of :casedText
   validates_format_of :text, :with => /\A[^!\$%\^&+\.,#]*\z/, :on => :create
+
+  before_validation :fix_casing
+
+  def fix_casing
+    self.casedText = self.text.clone
+    self.text.downcase!
+  end
 
 end
