bdf84430768344594224709f3cf08c0363d2f9c0
James Bowman <bowmessage@gmail.com>
2014-08-26 12:38:00 -0500

    Added compilation=true to precompilation.

--- RailsApp/config/environments/production.rb
+++ RailsApp/config/environments/production.rb
@@ -27,7 +27,8 @@ CollegeFeed::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,7 +61,6 @@ CollegeFeed::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
