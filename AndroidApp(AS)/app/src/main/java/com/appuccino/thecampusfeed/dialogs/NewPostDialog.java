4ee671976ea9c14b804b1e40516816cab586963d
Waylon Brown <waylonjbrown@gmail.com>
2014-09-07 21:16:28 -0500

    Using new Regex with NewPostDialog

--- AndroidApp(AS)/app/src/main/java/com/appuccino/thecampusfeed/dialogs/NewPostDialog.java
+++ AndroidApp(AS)/app/src/main/java/com/appuccino/thecampusfeed/dialogs/NewPostDialog.java
@@ -22,6 +22,8 @@ import com.appuccino.thecampusfeed.utils.NetWorker.MakePostTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NewPostDialog extends AlertDialog.Builder{
 	
@@ -84,103 +86,110 @@ public class NewPostDialog extends AlertDialog.Builder{
 	    }).show();
 	}
 
-	private void createDialog(View layout) {
-		dialog = create();
-		dialog.show();
-		
-		final EditText postMessage = (EditText)layout.findViewById(R.id.newPostMessage);
-		Button postButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
-    	postButton.setOnClickListener(new View.OnClickListener()
-    	{
-    		@Override
-			public void onClick(View v) 
-    		{				
-    			String thisString = postMessage.getText().toString().trim();
-    			if(thisString.length() >= MainActivity.MIN_POST_LENGTH)
-    			{
-    				Post newPost = new Post(thisString, selectedCollegeID);
-        			new MakePostTask(context, main).execute(newPost);
-        			dialog.dismiss();
-    			}
-    			else
-    			{
-    				Toast.makeText(context, "Post must be at least " + MainActivity.MIN_POST_LENGTH + " characters long.", Toast.LENGTH_LONG).show();
-    			}
-			}
-    	});
-    	
-    	TextView title = (TextView)layout.findViewById(R.id.newPostTitle);
-    	TextView college = (TextView)layout.findViewById(R.id.collegeText);
-    	postMessage.setTypeface(FontManager.light);
-    	college.setTypeface(FontManager.italic);
-    	title.setTypeface(FontManager.light);
-    	postButton.setTypeface(FontManager.light);
-    	
-    	//ensure keyboard is brought up when dialog shows
-    	postMessage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-    	    @Override
-    	    public void onFocusChange(View v, boolean hasFocus) {
-    	        if (hasFocus) {
-    	            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-    	        }
-    	    }
-    	});
-   
-    	final TextView tagsText = (TextView)layout.findViewById(R.id.newPostTagsText);
-    	tagsText.setTypeface(FontManager.light);
-    	
-    	//set listener for tags
-    	postMessage.addTextChangedListener(new TextWatcher(){
+    private void createDialog(View layout) {
+        dialog = create();
+        dialog.show();
 
-			@Override
-			public void afterTextChanged(Editable s) {
-			}
+        final EditText postMessage = (EditText)layout.findViewById(R.id.newPostMessage);
+        Button postButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        postButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String thisString = postMessage.getText().toString().trim();
+                if(thisString.length() >= MainActivity.MIN_POST_LENGTH)
+                {
+                    Post newPost = new Post(thisString, selectedCollegeID);
+                    new MakePostTask(context, main).execute(newPost);
+                    dialog.dismiss();
+                }
+                else
+                {
+                    Toast.makeText(context, "Post must be at least " + MainActivity.MIN_POST_LENGTH + " characters long.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-			}
+        TextView title = (TextView)layout.findViewById(R.id.newPostTitle);
+        TextView college = (TextView)layout.findViewById(R.id.collegeText);
+        postMessage.setTypeface(FontManager.light);
+        college.setTypeface(FontManager.italic);
+        title.setTypeface(FontManager.light);
+        postButton.setTypeface(FontManager.light);
 
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-				String message = postMessage.getText().toString();
-				String currentTags = "Tags: <font color='#33B5E5'>";
-				
-				String[] wordArray = message.split(" ");
-				if(wordArray.length > 0)
-				{
-					for(int i = 0; i < wordArray.length; i++)
-					{
-						//prevent indexoutofboundsexception
-						if(wordArray[i].length() > 0)
-						{
-							if(wordArray[i].substring(0, 1).equals("#") && wordArray[i].length() > 1 && !MainActivity.containsSymbols(wordArray[i].substring(1, wordArray[i].length())))
-							{
-								currentTags += wordArray[i] + " ";
-							}
-						}
-					}
-				}
-				
-				currentTags += "</font>";
-				//if there aren't any tags and view is shown, remove view
-				if(currentTags.equals("Tags: <font color='#33B5E5'></font>") && tagsText.isShown())
-				{
-					tagsText.setVisibility(View.GONE);
-				}					
-				else if(!currentTags.equals("Tags: <font color='#33B5E5'></font>") && !tagsText.isShown())
-				{
-					tagsText.setVisibility(View.VISIBLE);
-				}
-					
-				tagsText.setText(Html.fromHtml((currentTags)));
-			}
-    		
-    	});
-    	
-    	setupCollege(college);
-	}
+        //ensure keyboard is brought up when dialog shows
+        postMessage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                }
+            }
+        });
+
+        final TextView tagsText = (TextView)layout.findViewById(R.id.newPostTagsText);
+        tagsText.setTypeface(FontManager.light);
+
+        //set listener for tags
+        postMessage.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                String message = postMessage.getText().toString();
+                String currentTags = "Tags: <font color='#33B5E5'>";
+
+                String[] tagArray = parseTagsWithRegex(message);
+                if(tagArray.length > 0)
+                {
+                    for(int i = 0; i < tagArray.length; i++)
+                    {
+                        //prevent indexoutofboundsexception
+                        if(tagArray[i].length() > 0)
+                        {
+                            currentTags += tagArray[i] + " ";
+                        }
+                    }
+                }
+
+                currentTags += "</font>";
+                //if there aren't any tags and view is shown, remove view
+                if(currentTags.equals("Tags: <font color='#33B5E5'></font>") && tagsText.isShown())
+                {
+                    tagsText.setVisibility(View.GONE);
+                }
+                else if(!currentTags.equals("Tags: <font color='#33B5E5'></font>") && !tagsText.isShown())
+                {
+                    tagsText.setVisibility(View.VISIBLE);
+                }
+
+                tagsText.setText(Html.fromHtml((currentTags)));
+            }
+
+        });
+
+        setupCollege(college);
+    }
+
+    private String[] parseTagsWithRegex(String message) {
+        List<String> returnList = new ArrayList<String>();
+        Matcher m = Pattern.compile("#[A-Za-z0-9_]{3,139}").matcher(message);
+        while (m.find()){
+            returnList.add(m.group());
+        }
+
+        return returnList.toArray(new String[returnList.size()]);
+    }
 
     public boolean isShowing(){
         if(dialog == null)
