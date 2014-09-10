db500031fbc8e8b1d4818c11c0a9dc31f11c5494
James Bowman <bowmessage@gmail.com>
2014-09-10 17:24:06 -0500

    Don't compile all css assets, just those in assets folder.

--- RailsApp/config/environments/production.rb
+++ RailsApp/config/environments/production.rb
@@ -29,7 +29,7 @@ CollegeFeed::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.enabled = true
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile =  ['*.js']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
