11547d5a5abfdea56b5548767f2553550c876407
James Bowman <bowmessage@gmail.com>
2014-09-10 21:21:48 -0500

    Prevent worker threads from launching in rake task.

--- RailsApp/config/initializers/workers.rb
+++ RailsApp/config/initializers/workers.rb
@@ -1,22 +1,24 @@
 require 'CommentScoreWorker'
 require 'PostScoreWorker'
 
-Thread.new do
-  while true
-    Post.all.each{ |post|
-      PostScoreWorker.perform_async post.id
-      sleep 1
-    }
-    sleep 10
+unless File.basename($0) == "rake"
+  Thread.new do
+    while true
+      Post.all.each{ |post|
+        PostScoreWorker.perform_async post.id
+        sleep 1
+      }
+      sleep 10
+    end
   end
-end
 
-Thread.new do
-  while true
-    Comment.all.each{ |comment|
-      CommentScoreWorker.perform_async comment.id
-      sleep 1
-    }
-    sleep 10
+  Thread.new do
+    while true
+      Comment.all.each{ |comment|
+        CommentScoreWorker.perform_async comment.id
+        sleep 1
+      }
+      sleep 10
+    end
   end
 end
