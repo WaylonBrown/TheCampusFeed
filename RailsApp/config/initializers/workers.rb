c292c5877ed944095179cd43aa21ae36227ebc00
James Bowman <bowmessage@gmail.com>
2014-09-29 23:08:34 -0500

    Syntax error

--- RailsApp/config/initializers/workers.rb
+++ RailsApp/config/initializers/workers.rb
@@ -6,9 +6,9 @@ unless File.basename($0) == "rake"
     while true
       sleep 10
       @posts = []
-      ActiveRecord::with_connection({|con|
+      ActiveRecord::with_connection{|con|
         @posts = Post.all
-      })
+      }
       @posts.each{ |post|
         PostScoreWorker.perform_async post.id
         sleep 0.3
@@ -20,9 +20,9 @@ unless File.basename($0) == "rake"
     while true
       sleep 10
       @comments = []
-      ActiveRecord::with_connection({|con|
+      ActiveRecord::with_connection{|con|
         @comments = Comment.all
-      })
+      }
       @comments.each{ |comment|
         CommentScoreWorker.perform_async comment.id
         sleep 0.3
@@ -34,9 +34,9 @@ unless File.basename($0) == "rake"
     while true
       sleep 10
       @colleges = []
-      ActiveRecord::with_connection({|con|
+      ActiveRecord::with_connection{|con|
         @colleges = College.all
-      })
+      }
       @colleges.each{ |college|
         CollegeRecentPostNumberWorker.perform_async college.id
         sleep 0.3
