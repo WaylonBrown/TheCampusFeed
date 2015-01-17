53425e956bbaf1eaf5f6cdcdcf4ca90de14e19a5
James Bowman <bowmessage@gmail.com>
2015-01-17 03:59:12 -0600

    Staging deploy finally looking good. Goodnight.

--- RailsApp/config/deploy.rb
+++ RailsApp/config/deploy.rb
@@ -44,11 +44,12 @@ before 'deploy:assets:precompile', :symlink_config_files
 
 desc "Link shared files"
 task :symlink_config_files do
-  symlinks = {
-    "#{shared_path}/config/database.yml" => "#{release_path}/config/database.yml",
-    "#{shared_path}/config/local_env.yml" => "#{release_path}/config/local_env.yml"
-  }
-  run symlinks.map{|from, to| "ln -nfs #{from} #{to}"}.join(" && ")
+  on roles(:web) do
+    symlinks = {
+      "#{shared_path}/config/local_env.yml" => "#{release_path}/config/local_env.yml"
+    }
+    execute symlinks.map{|from, to| "ln -nfs #{from} #{to}"}.join(" && ")
+  end
 end
 
 namespace :deploy do
